5ca299c6e8faacc5d621320d186451ac2cd62b04
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-01 19:50:16 +0100

    quarter files upload with CSV initial conversion

--- dashboard/pages/signals.py
+++ dashboard/pages/signals.py
@@ -3,7 +3,7 @@ import shutil
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.conf import settings
-from .models import Quarter
+from .models import Quarter, QuarterFile
 
 @receiver(post_save, sender=Quarter)
 def create_quarter_directory(sender, instance, created, **kwargs):
@@ -17,3 +17,8 @@ def delete_quarter_directory(sender, instance, **kwargs):
     if os.path.isdir(path):
         shutil.rmtree(path)
         
+
+@receiver(post_save, sender=QuarterFile)
+def process_file_on_upload(sender, instance, created, **kwargs):
+    if created:
+        instance.process_and_store_csv()
