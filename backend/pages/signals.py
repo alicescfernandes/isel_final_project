e512df7217fa290b1eef3efb295e181c65032b74
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-08 19:28:16 +0100

    adds hard delelte flag

--- backend/pages/signals.py
+++ backend/pages/signals.py
@@ -5,29 +5,32 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Quarter, ExcellFile, CSVFile
 
+""" 
+# TODO: Remove this on cleanup
 @receiver(post_save, sender=Quarter)
 def create_quarter_directory(sender, instance, created, **kwargs):
     if created:
         path = os.path.join(settings.MEDIA_ROOT, 'uploads', str(instance.uuid))
         os.makedirs(path, exist_ok=True)
+"""
 
 @receiver(post_save, sender=ExcellFile)
 def process_file_on_upload(sender, instance, created, **kwargs):
     instance.process_and_store_csv()
 
-# TODO: Remove this code, this is here mostly for development purposes to avoid confusion
-@receiver(post_delete, sender=Quarter)
-def delete_quarter_directory(sender, instance, **kwargs):
-    path = os.path.join(settings.MEDIA_ROOT, 'uploads', str(instance.uuid))
-    if os.path.isdir(path):
-        shutil.rmtree(path)
-        
-@receiver(post_delete, sender=ExcellFile)
-def delete_files_on_ExcellFile_delete(sender, instance, **kwargs):
-    if instance.file and os.path.exists(instance.file.path):
-        os.remove(instance.file.path)
+if settings.HARD_DELETE == True:
+    @receiver(post_delete, sender=Quarter)
+    def delete_quarter_directory(sender, instance, **kwargs):
+        path = os.path.join(settings.MEDIA_ROOT, 'uploads', str(instance.uuid))
+        if os.path.isdir(path):
+            shutil.rmtree(path)
             
-@receiver(post_delete, sender=CSVFile)
-def delete_csv_file_from_disk(sender, instance, **kwargs):
-    if instance.csv_path and os.path.exists(instance.csv_path):
-        os.remove(instance.csv_path)
+    @receiver(post_delete, sender=ExcellFile)
+    def delete_files_on_ExcellFile_delete(sender, instance, **kwargs):
+        if instance.file and os.path.exists(instance.file.path):
+            os.remove(instance.file.path)
+                
+    @receiver(post_delete, sender=CSVFile)
+    def delete_csv_file_from_disk(sender, instance, **kwargs):
+        if instance.csv_path and os.path.exists(instance.csv_path):
+            os.remove(instance.csv_path)
