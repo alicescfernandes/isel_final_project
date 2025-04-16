6cc66a82e0bf3ddd1cc19a916c6b8ee6c444a33b
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-16 19:37:21 +0100

    remove build folder; fix build script

--- docs/final_report/docker.sh
+++ docs/final_report/docker.sh
@@ -1,3 +1,5 @@
 # Run this with powershell
-docker build -t pdflatex .
-docker run --rm -v "$(pwd):/data" -w /data pdflatex
+docker build -t compiler_image .
+docker run --rm -v "$(pwd):/data" -w /data compiler_image
+
+# docker run --rm -it -v "${PWD}:/data" -w /data compiler_image sh
