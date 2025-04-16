e7fcbdee6992b9714db998ae45e76273f716c8a4
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-16 19:07:13 +0100

    more writing

--- docs/final_report/build.sh
+++ docs/final_report/build.sh
@@ -1,11 +1,18 @@
-rm -rf build
 mkdir -p build
 chmod u+w build
+cp XBib.bib build/
+cp apalikeMy.bst build/
+
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
-cd build && makeglossaries 00_PRJ_main
+
+cd build
+makeglossaries 00_PRJ_main
+bibtex 00_PRJ_main
 cd ..
+
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 
 rm final_report.pdf
 mv build/00_PRJ_main.pdf final_report.pdf
+# rm build -rf
--- docs/final_report/docker.sh
+++ docs/final_report/docker.sh
@@ -1,12 +1,3 @@
-# docker run --rm -v $(pwd):/data texlive/texlive pdflatex data/00_PRJ_main.tex
-
-
-# docker build -t my-latex-image .
-# docker run --rm -v $(pwd):/data my-latex-image
-
-# PS
+# Run this with powershell
 docker build -t pdflatex .
-docker run --rm -it -v "${PWD}:/data" -w /data pdflatex sh
-
-
-# docker build --no-cache -t your-image-name .
+docker run --rm -v "$(pwd):/data" -w /data pdflatex
