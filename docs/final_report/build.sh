3adbba4a5eff4b7bcde5d1f856af387e9480083c
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-16 15:12:18 +0100

    adds alot of text

--- docs/final_report/build.sh
+++ docs/final_report/build.sh
@@ -5,5 +5,7 @@ pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 cd build && makeglossaries 00_PRJ_main
 cd ..
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
+pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 
+rm final_report.pdf
 mv build/00_PRJ_main.pdf final_report.pdf
