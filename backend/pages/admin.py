1032b524f81274044d0d66653cc71c20e1cf818f
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-18 18:13:38 +0100

    adds user to admin

--- backend/pages/admin.py
+++ backend/pages/admin.py
@@ -7,18 +7,18 @@ class QuarterAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'number', 'uuid', 'created_at')
     search_fields = ('number', 'uuid')
     ordering = ('-created_at',)
-    exclude = ('user',)  # 🔹 Oculta o campo no formulário
+    exclude = ('user',)
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:
-            obj.user = request.user  # 🔹 Atribui o utilizador logado
+            obj.user = request.user 
         super().save_model(request, obj, form, change)
 
-    # def get_queryset(self, request):
-    #     qs = super().get_queryset(request)
-    #     if request.user.is_superuser:
-    #         return qs
-    #     return qs.filter(user=request.user)  # 🔹 Só mostra os próprios objetos
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
 @admin.register(CSVData)
 class CSVDataAdmin(admin.ModelAdmin):
     list_display = ('sheet_name_slug', 'user', 'sheet_name', 'is_current', 'csv_path', 'quarter_file', 'quarter_uuid')
@@ -29,11 +29,11 @@ class CSVDataAdmin(admin.ModelAdmin):
             obj.user = request.user
         super().save_model(request, obj, form, change)
 
-    # def get_queryset(self, request):
-    #     qs = super().get_queryset(request)
-    #     if request.user.is_superuser:
-    #         return qs
-    #     return qs.filter(user=request.user)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
 @admin.register(ExcelFile)
 class ExcelFileAdmin(admin.ModelAdmin):
     list_display = ('quarter', 'user', 'is_processed', 'section_name', 'uploaded_at')
@@ -44,8 +44,8 @@ class ExcelFileAdmin(admin.ModelAdmin):
             obj.user = request.user
         super().save_model(request, obj, form, change)
 
-    # def get_queryset(self, request):
-    #     qs = super().get_queryset(request)
-    #     if request.user.is_superuser:
-    #         return qs
-    #     return qs.filter(user=request.user)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
