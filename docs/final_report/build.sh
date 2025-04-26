aeaeabb13667a60b193a30c2174a7d95f544c728
Alice Fernandes <alicescfernandes@gmail.com>
2025-04-26 15:33:12 +0100

    more report

--- docs/final_report/build.sh
+++ docs/final_report/build.sh
@@ -7,13 +7,12 @@ cp apalikeMy.bst build/
 
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 
-cd build
- makeglossaries 00_PRJ_main
- bibtex 00_PRJ_main
- cd ..
+makeglossaries -d build 00_PRJ_main
+bibtex build/00_PRJ_main
 
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
 pdflatex --interaction=nonstopmode -output-directory=build 00_PRJ_main.tex
+
 
 mv build/00_PRJ_main.pdf build/final_report.pdf
 
